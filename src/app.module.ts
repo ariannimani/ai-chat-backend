835ad91d71dc7b0c0eb4160bc6523389407a7199
Ilir Gruda <ilir_gruda@hotmail.com>
2025-07-27 14:45:40 +0200

    missing ai module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,19 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
-import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommandModule } from 'nestjs-command';
+import { AiModule } from './ai/ai.module';
 import { AuthModule } from './auth/auth.module';
-import { RoomsModule } from './rooms/rooms.module';
 import { ChatsModule } from './chats/chats.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { UsersModule } from './users/users.module';
 
-import { SupabaseModule } from './config/supabase/supabase.module';
-import { SupabaseAuthGuard } from './config/guard/supabase-auth.guard';
-import { User } from './users/entities/user.entity';
-import { Room } from './rooms/entities/room.entity';
 import { Chat } from './chats/entities/chat.entity';
+import { SupabaseAuthGuard } from './config/guard/supabase-auth.guard';
+import { SupabaseModule } from './config/supabase/supabase.module';
+import { Room } from './rooms/entities/room.entity';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
@@ -35,6 +36,7 @@ import { Chat } from './chats/entities/chat.entity';
 
     SupabaseModule,
     CommandModule,
+    AiModule,
     UsersModule,
     AuthModule,
     RoomsModule,
